eb2d28bdb4683fdc1a906abdf7f4129f8e8c7311
Chris Gnam <chrisrsg@gmail.com>
2026-01-19 12:25:16 -0500

    Add RayHit

---
+++ include/huira/core/ray_hit.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <limits>
+#include <array>
+
+#include "huira/core/types.hpp"
+#include "huira/detail/concepts/numeric_concepts.hpp"
+#include "huira/detail/concepts/spectral_concepts.hpp"
+
+namespace huira {
+    template <IsSpectral TSpectral, IsFloatingPoint TFloat>
+    struct RayHit {
+        TFloat t = std::numeric_limits<TFloat>::infinity();
+
+        std::array<Vertex<TSpectral, TFloat>, 3> vert;
+        Vec3<float> face_normal;
+        std::array<TFloat, 3> w;
+    };
+
+    template <IsFloatingPoint T>
+    inline Vec3<T> offset_intersection_(Vec3<T> intersection, const Vec3<T>& N);
+}
+
+#include "huira_impl/core/ray_hit.ipp"
