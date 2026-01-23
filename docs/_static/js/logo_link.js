654ed220ebe5e87529edfa8c59dc131993fc7908
Chris Gnam <chrisrsg@gmail.com>
2026-01-22 23:18:19 -0500

    docs: Change theme to furo

--- docs/_static/js/logo_link.js
+++ docs/_static/js/logo_link.js
@@ -1,6 +1,6 @@
-// Make logo link to main website
+// Make logo link to main website (Furo theme)
 document.addEventListener('DOMContentLoaded', function() {
-    var logoLink = document.querySelector('.wy-side-nav-search > a');
+    var logoLink = document.querySelector('.sidebar-brand');
     if (logoLink) {
         logoLink.href = 'https://www.huira.space';
         logoLink.target = '_blank';
