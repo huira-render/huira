afe1ad9ff1df6026de0eac766b4003e0b76bf8e9
Chris Gnam <chrisrsg@gmail.com>
2026-02-17 17:45:15 -0500

    feat: Add python bindings to conda packaging

--- packaging/recipe/bld.bat
+++ packaging/recipe/bld.bat
@@ -7,7 +7,8 @@ cmake -B build ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-    -DHUIRA_APPS=ON
+    -DHUIRA_APPS=ON ^
+    -DHUIRA_PYTHON=ON
 if errorlevel 1 exit 1
 
 cmake --build build --parallel %CPU_COUNT%
