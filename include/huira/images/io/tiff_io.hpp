d75a0d42756f7a724f8f196ad6e94dcae028d1f8
Chris Gnam <chrisrsg@gmail.com>
2026-02-21 14:37:33 -0500

    feat: Add simple TIFF reading

--- include/huira/images/io/tiff_io.hpp
+++ include/huira/images/io/tiff_io.hpp
@@ -7,8 +7,8 @@
 #include "huira/images/image.hpp"
 
 namespace huira {
-    template <IsImagePixel T>
-    std::pair<Image<T>, Image<float>> read_image_tiff(const fs::path& filepath);
+    std::pair<Image<RGB>, Image<float>> read_image_tiff_rgb(const fs::path& filepath, bool read_alpha = true);
+    std::pair<Image<float>, Image<float>> read_image_tiff_mono(const fs::path& filepath, bool read_alpha = true);
 }
 
 #include "huira_impl/images/io/tiff_io.ipp"
