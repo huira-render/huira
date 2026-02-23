24efd39c9707c3c93c501ef0eb59ff594ce5cf4b
Chris Gnam <chrisrsg@gmail.com>
2026-02-23 09:06:17 +0000

    feat: Add texture and material handles

--- include/huira/handles/material_handle.hpp
+++ include/huira/handles/material_handle.hpp
@@ -2,6 +2,7 @@
 
 #include "huira/core/concepts/spectral_concepts.hpp"
 #include "huira/handles/handle.hpp"
+#include "huira/handles/texture_handle.hpp"
 #include "huira/materials/material.hpp"
 
 namespace huira {
@@ -19,6 +20,26 @@ namespace huira {
         MaterialHandle() = delete;
         using Handle<Material<TSpectral>>::Handle;
 
+        void set_albedo(const TextureHandle<TSpectral>& albedo_texture);
+        void set_albedo_factor(TSpectral albedo_factor);
+        void reset_albedo();
 
+        void set_metallic_image(const TextureHandle<float>& metallic_texture);
+        void set_metallic_factor(float metallic_factor);
+        void reset_metallic();
+
+        void set_roughness_image(const TextureHandle<float>& roughness_texture);
+        void set_roughness_factor(float roughness_factor);
+        void reset_roughness();
+
+        void set_normal_image(const TextureHandle<Vec3<float>>& normal_texture);
+        void set_normal_factor(float normal_factor);
+        void reset_normal();
+
+        void set_emissive_image(const TextureHandle<TSpectral>& emissive_texture);
+        void set_emissive_factor(TSpectral emissive_factor);
+        void reset_emissive();
     };
 }
+
+#include "huira_impl/handles/material_handle.ipp"
--- include/huira/handles/texture_handle.hpp
+++ include/huira/handles/texture_handle.hpp
@@ -13,12 +13,15 @@ namespace huira {
      *
      * @tparam TSpectral The spectral type
      */
-    template <typename T>
-    class TextureHandle : public Handle<Texture<T>> {
+    template <typename TPixel>
+    class TextureHandle : public Handle<Texture<TPixel>> {
     public:
         TextureHandle() = delete;
-        using Handle<Texture<T>>::Handle;
+        using Handle<Texture<TPixel>>::Handle;
 
+        Image<TPixel>* image() noexcept { return this->get_()->image(); }
+        const Image<TPixel>* image() const noexcept { return this->get_()->image(); }
 
+        Resolution resolution() const noexcept { return this->get_()->resolution(); }
     };
 }
