4c2baac714cfef90f1717d909cfe3330572513be
Chris Gnam <chrisrsg@gmail.com>
2026-03-12 08:04:52 -0400

    fix: Update FITS I/O to properly handle images with inf - Update portfile.cmake to better reflect port deployed on vcpkg - Update packaging test to call more of the library

--- tests/packaging/main.cpp
+++ tests/packaging/main.cpp
@@ -11,7 +11,22 @@ int main() {
 
     // Set a time:
     huira::Time time("2016-09-19T16:22:05.728");
+    huira::Interval exposure_interval = huira::Interval::from_centered(time, 9.984285275_s);
 
+    // Create the renderer
+    huira::Renderer<TSpectral> renderer;
+
+    //  Create a scene view over the exposure interval
+    auto scene_view = huira::SceneView<TSpectral>(scene, exposure_interval, mapcam, huira::ObservationMode::ABERRATED_STATE, 3);
+
+    // Render the current scene view
+    renderer.render(scene_view, frame_buffer);
+    renderer.set_samples_per_pixel(1);
+
+    auto frame_buffer = camera_model.make_frame_buffer();
+    frame_buffer.enable_depth();
+
+    huira::write_image_fits("demo.fits", frame_buffer.depth(), 16);
 
     std::cout << "Huira Packaging test PASSED" << std::endl;
     
