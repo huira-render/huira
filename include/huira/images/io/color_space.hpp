1ac97bcac573c24ad89d8bbf33ac2c8ade52558a
Chris Gnam <chrisrsg@gmail.com>
2026-04-19 21:10:02 -0400

    fix: Apply sRGB-to-linear conversion for color textures and add linear-to-sRGB output utility
    - Replace `is_normal_map` parameter with `is_color_data` in `load_material_texture_` to
      convert sRGB color textures to linear space on load
    - Detect normal maps by texture type (`aiTextureType_NORMALS`) instead of a flag
    - Add `linear_to_srgb` overloads for `ImageBundle<RGB>` and `ImageBundle<float>`
    - Apply sRGB encoding to earth example output
    - Update known bugs in Python bindings README

--- include/huira/images/io/color_space.hpp
+++ include/huira/images/io/color_space.hpp
@@ -33,4 +33,35 @@ namespace huira {
         return 1.055f * std::pow(linear, 1.0f / 2.4f) - 0.055f;
     }
 
+    ImageBundle<RGB> linear_to_srgb(const ImageBundle<RGB>& linear_bundle)
+    {
+        Image<RGB> srgb_image(linear_bundle.image.width(), linear_bundle.image.height());
+        for (int y = 0; y < linear_bundle.image.height(); ++y) {
+            for (int x = 0; x < linear_bundle.image.width(); ++x) {
+                const RGB& linear_pixel = linear_bundle.image(x, y);
+                srgb_image(x, y) = RGB{
+                    linear_to_srgb(linear_pixel[0]),
+                    linear_to_srgb(linear_pixel[1]),
+                    linear_to_srgb(linear_pixel[2])
+                };
+            }
+        }
+        ImageBundle<RGB> output_bundle(std::move(srgb_image), linear_bundle.alpha);
+        output_bundle.color_space = ColorSpaceHint::sRGB;
+        return output_bundle;
+    }
+
+    ImageBundle<float> linear_to_srgb(const ImageBundle<float>& linear_bundle)
+    {
+        Image<float> srgb_image(linear_bundle.image.width(), linear_bundle.image.height());
+        for (int y = 0; y < linear_bundle.image.height(); ++y) {
+            for (int x = 0; x < linear_bundle.image.width(); ++x) {
+                const float& linear_pixel = linear_bundle.image(x, y);
+                srgb_image(x, y) = linear_to_srgb(linear_pixel);
+            }
+        }
+        ImageBundle<float> output_bundle(std::move(srgb_image), linear_bundle.alpha);
+        output_bundle.color_space = ColorSpaceHint::sRGB;
+        return output_bundle;
+    }
 }
